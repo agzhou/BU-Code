8787df41398b45a3e83950ed9f3fa341ccb6ca0d
agzhou <zhoualleng@gmail.com>
2025-01-18 16:53:32 -0500

    save IQ after each recon individually

--- Allen code/Verasonics/condenseRcvFiles.m
+++ Allen code/Verasonics/condenseRcvFiles.m
@@ -24,16 +24,22 @@ numFiles = 315; % regexp to get last file number?
 P.numSubFrames = P.numFramesPerBuffer; % temporary fix
 IQstack = []; % Create empty variable to append the data to
 
+mkdir(datapath, 'IQ data')
+filename_structure_IQ = [probe_name, '-IQ-', num2str(P.maxAngle), '-', num2str(P.na), '-', num2str(P.PRF), '-', num2str(P.frameRate), '-', num2str(P.numFramesPerBuffer), '-'];
+
+
 % for filenum = 1:numFiles
 for filenum = 1:2
     load([datapath, filename_structure, num2str(filenum), '.mat'])
     IQ = LA_DAS(RcvData, P, P.wl/2);
     disp(strcat("recon on buffer ", num2str(filenum), " done"))
     IQstack = cat(4, IQstack, IQ); % concatenate along the frame dimension
+
+    save([datapath, 'IQ data\', filename_structure_IQ, num2str(filenum)], 'IQ', "-v6")
 end
 % %%
 % [zs, xs, na, nf] = size(IQstack);
 % test = reshape(IQstack, [zs/numFiles, xs, na, nf*numFiles]);
 %%
-IQ_coherent_sum = squeeze(sum(IQ, 4));
+IQ_coherent_sum = squeeze(sum(IQstack, 3)); % sum along angle dimension
 I_coherent_sum = abs(IQ_coherent_sum);
