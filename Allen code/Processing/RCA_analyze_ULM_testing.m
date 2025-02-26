25c27d2f93bcd36f1ea94a90fae574e22f66e2f3
agzhou <zhoualleng@gmail.com>
2025-02-26 17:29:49 -0500

    adding denoising for the 3D ULM localization

--- Allen code/Processing/RCA_analyze_ULM_testing.m
+++ Allen code/Processing/RCA_analyze_ULM_testing.m
@@ -107,18 +107,26 @@ for filenum = 30
 %     tic
     [IQf] = applySVs2D(IQ, PP, EVs, V_sort, sv_threshold_lower, sv_threshold_upper);
     disp('SVD filtered images put together')
-
-    % Testing
-    test = squeeze(abs(IQf(:, :, :, 1)) ./ max(abs(IQf(:, :, :, 1)), [], 'all'));
-    IQf_dn = NLMF(test);
-    IQf_dn_rfn = imresize3(IQf_dn, [size(IQf_dn, 1) * imgRefinementFactor(1), size(IQf_dn, 2) * imgRefinementFactor(2), size(IQf_dn, 3) * imgRefinementFactor(3)]);
-    testXC = normxcorr3(abs(refPSF), IQf_dn_rfn);
+    
+    % Denoise
+    IQf_dn = zeros(size(IQf));
+    parfor f = 1:size(IQf, 4) % Go through each frame and denoise
+%         tic
+        IQf_scaled_temp = squeeze(abs(IQf(:, :, :, f)) ./ max(abs(IQf(:, :, :, f)), [], 'all'));
+        IQf_dn(:, :, :, f) = NLMF(IQf_scaled_temp);
+%         toc
+    end
+%     IQf_dn_rfn = 
+%     parfor f = 1:size(IQf_dn)
+%         IQf_dn_rfn = imresize3(IQf_dn, [size(IQf_dn, 1) * imgRefinementFactor(1), size(IQf_dn, 2) * imgRefinementFactor(2), size(IQf_dn, 3) * imgRefinementFactor(3)]);
+%     end
+%     testXC = normxcorr3(abs(refPSF), IQf_dn_rfn);
 %     toc
     clear PP EVs V_sort
 
 %     save([savepath, 'Filtered-Data-', num2str(filenum)], 'IQr', 'PP', 'EVs', 'V_sort', 'IQf', "-v6")
 %     [centers, refIQs, XC] = localizeBubbles3D(IQf, refPSF, range, imgRefinementFactor, XCThreshold);
-    [centers, ~, ~, XCThresholdAdaptive] = localizeBubbles3D(IQf, refPSF, range, imgRefinementFactor, XCThresholdFactor);
+    [centers, ~, ~, XCThresholdAdaptive] = localizeBubbles3D(IQf_dn, refPSF, range, imgRefinementFactor, XCThresholdFactor);
 %     [coords, img_size, XCThresholdsAdaptive] = localizeBubbles3D_framewise(IQf, refPSF, range, imgRefinementFactor, XCThresholdFactor);
 %     save([savepath, 'IQf-', num2str(filenum)], 'IQf', "-v6")
 
