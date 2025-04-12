91e1c5ab01d2fa02fca588b7001cb215c4b7503f
agzhou <zhoualleng@gmail.com>
2025-04-12 00:13:02 -0400

    Added a parameter update function for the unstacked data, and added a Verasonics recon script for the unstacked frame data

--- Allen code/Reconstruction/updateParams_unstackedFrames.m
+++ Allen code/Reconstruction/updateParams_unstackedFrames.m
@@ -1,15 +1,6 @@
-function [RcvData_unstacked, P_unstacked] = unstackFrames(RcvData, P)
-    %% Use parallel processing for speed
-    % https://www.mathworks.com/matlabcentral/answers/91744-how-can-i-check-if-matlabpool-is-running-when-using-parallel-computing-toolbox
+function [P_unstacked] = updateParams_unstackedFrames(P)
     
-%     pp = gcp('nocreate');
-%     if isempty(pp)
-%         % There is no parallel pool
-%         parpool LocalProfile1
-%     
-%     end
-
-    %%
+    % Derive some parameters
     try rcvChunkSize = P.rcvChunkSize;
     catch
     end
@@ -23,29 +14,8 @@ function [RcvData_unstacked, P_unstacked] = unstackFrames(RcvData, P)
     nspa = P.Receive(1).endSample; % # samples per acquisition
     nspf = nspa * P.na * 2; % # samples per frame
     
-    RcvData_unstacked = zeros(nspf, P.Resource.Parameters.numRcvChannels, nsf, 'int16');
 
-    % Unstack the frames
-%     RcvData_unstacked = reshape(RcvData, [nspf, P.Resource.Parameters.numRcvChannels, nsf]);
-    
-    if length(size(RcvData)) == 2 % all frames are stacked
-        for n = 1:nsf
-            RcvData_unstacked(:, :, n) = RcvData((n-1) * nspf + 1 : n * nspf, :);
-        end
-    else % if only some frames are stacked, RcvData should have 3 dimensions
-        for n = 1:rcvChunkSize:nsf
-            for c = 0:rcvChunkSize - 1
-                RcvData_unstacked(:, :, n + c) = RcvData((n-1) * nspf + 1 : n * nspf, :, n + c);
-            end
-    
-        end
-    end
-    
-    % Remap according to the element - channel map (comment the line below
-    % if using Verasonics recon)
-%     RcvData_unstacked = RcvData_unstacked(:, P.Trans.Connector, :);
-
-    %% Redefine the parameters structure with the unstacked parameters
+    % Redefine the parameters structure with the unstacked parameters
     P_unstacked = P;
     P_unstacked.numFramesPerBuffer = nsf;
     P_unstacked.Resource.RcvBuffer.numFrames = nsf;
@@ -53,7 +23,6 @@ function [RcvData_unstacked, P_unstacked] = unstackFrames(RcvData, P)
     P_unstacked.Receive = updateReceiveStructure(P_unstacked);
 end
 
-% figure; imagesc(abs(squeeze(double(RcvData_unstacked(:, :, 1))) .^ 0.5))
 
 %% Helper function for setting the Receive structure
 % P_unstacked.Receive(1).
@@ -76,7 +45,6 @@ function [Receive] = updateReceiveStructure(P_unstacked)
     for nf = 1:P_unstacked.numFramesPerBuffer
         % Move points after all the acquisitions for one frame
 %         Receive(j).callMediaFunc = movePointsOrNot;
-    %     Receive(j).mode = 0; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         an = 0;
 
         for n = 1:P_unstacked.na
