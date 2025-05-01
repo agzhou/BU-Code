aa439c0a8cccc3570a6e457723298465b64c9fc1
agzhou <zhoualleng@gmail.com>
2025-05-01 18:35:32 -0400

    Added more stuff to the fUS trial averaging stuff, Added a 3D rotating view gif maker

--- Allen code/Processing/generateHRF.m
+++ Allen code/Processing/generateHRF.m
@@ -18,5 +18,29 @@ c = 1;
 
 t = 0:duration;
 % HRF = 1 .* (t - delay_response).^2 .* exp(-b .* (t - delay_response).^c); % .* heaviside(t - delay_response)
-HRF = gampdf(t, 3, 5);
-figure; plot(HRF)
+% HRF = gampdf(t, 3, 5);
+
+alpha = 5;
+lambda = 0.001;
+% gamma = t .^ (alpha-1) .* exp(-lambda .* t) .* lambda .^ alpha ./ factorial(alpha-1);
+% HRF = (t - delay_response) .^ (alpha-1) .* exp(-lambda .* (t - delay_response)) .* lambda .^ alpha ./ factorial(alpha-1);
+
+% figure; plot(gamma)
+
+%%
+GF = @(t, alpha, lambda) t .^ (alpha-1) .* exp(-lambda .* t) .* lambda .^ alpha ./ factorial(alpha-1);
+
+alpha = 4;
+lambda = 0.005;
+
+stim_1trial = TD.airPuffOutput(trial_windows{1});
+gamma = GF(1:length(stim_1trial), alpha, lambda);
+% HRF = conv(stim_1trial, gamma, 'same');
+HRF = conv(stim_1trial, gamma);
+%
+figure
+plot(stim_1trial)
+hold on
+plot(gamma)
+plot(HRF)
+hold off
