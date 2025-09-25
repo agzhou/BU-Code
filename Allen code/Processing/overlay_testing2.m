bbfd085e66ec4aabb8781bc165277cc7be3d3d8c
agzhou <zhoualleng@gmail.com>
2025-09-25 15:19:51 -0400

    Update overlay_testing2.m

--- Allen code/Processing/overlay_testing2.m
+++ Allen code/Processing/overlay_testing2.m
@@ -1,6 +1,9 @@
 % Description:
 %       Overlay 3D activation maps onto a template (like CBV index)
 
+%% Load data (manual for now)
+% load('J:\08-28-2025 fUS activation map plotting\Trials 1 to 13\activation_maps.mat')
+load('J:\08-28-2025 fUS activation map plotting\Trials 1 to 13 diff proc\activation_maps.mat')
 %%
 % figure; imagesc(squeeze(max(CBVi_allSF_avg(:, :, :), [], 1) .^ 0.5)'); colormap gray
 
@@ -16,8 +19,12 @@ figure; imagesc(squeeze(max(test(:, :, :), [], 1) .^ 0.5)'); colormap gray
 cmap = colormap_ULM;
 
 % compareVolumes(CBVi_allSF_avg, am_rPDI, 'gray', cmap)
-compareVolumes(CBVi_allSF_avg .^ 0.5, am_rPDI)
-% compareVolumes(CBVi_allSF_avg .^ 1, am_rPDI)
+
+
+% compareVolumes(CBVi_allSF_avg .^ 0.5, am_rPDI)
+% compareVolumes(CBVi_allSF_avg .^ 0.5, am_rCBV)
+compareVolumes(CBVi_allSF_avg .^ 0.5, am_rCBFspeed .^ 1)
+
 % compareVolumes(test, am_rPDI)
 
 % compareVolumes(CBVi_allSF_avg, am_rCBV)
@@ -52,9 +59,14 @@ function compareVolumes(vol1, vol2, varargin) % Can change this so it has a cell
     %         Alphamap = "linear");   
 
     grid_alphamap = linspace(0, 1, 256)';
+    shift_factor = 0.1;
     % ramp_alphamap = grid_alphamap - round(length(grid_alphamap) ./ 4);
-    ramp_alphamap = grid_alphamap - max(grid_alphamap) .* 0.1;
+    ramp_alphamap = grid_alphamap - max(grid_alphamap) .* shift_factor;
     ramp_alphamap(ramp_alphamap < 0) = 0;
+
+    % normalize
+    max_factor = 1;
+    ramp_alphamap(ramp_alphamap > 0) = ramp_alphamap(ramp_alphamap > 0) ./ max(ramp_alphamap) .* max_factor;
 
     volshow(vol1 .^ 1, 'Colormap', cmap1, ...
             Parent = viewerThresholded, ...
@@ -62,17 +74,26 @@ function compareVolumes(vol1, vol2, varargin) % Can change this so it has a cell
             SpecularReflectance = 0.8, ...
             Alphamap = ramp_alphamap);
             % Alphamap = "linear");   
+ 
+    shift_factor2 = 0.4;
+    ramp_alphamap2 = grid_alphamap - max(grid_alphamap) .* shift_factor2;
+    ramp_alphamap2(ramp_alphamap2 < 0) = 0;
 
-    % volshow(vol1 .^ 1, 'Colormap', cmap1, ...
-    %         Parent = viewerThresholded, ...
-    %         RenderingStyle = "Isosurface", ...
-    %         IsosurfaceValue = 0.10, ...
-    %         IsosurfaceAlpha = 0.8, ...
-    %         Alphamap = "linear");   
+    % normalize
+    max_factor2 = 1;
+    ramp_alphamap2(ramp_alphamap2 > 0) = ramp_alphamap2(ramp_alphamap2 > 0) ./ max(ramp_alphamap2) .* max_factor2;
 
+    % volshow(vol2 .^ 1, Parent = viewerThresholded, ...
+    %         RenderingStyle = "MaximumIntensityProjection", ...
+    %         Colormap = cmap2, ...
+    %         Alphamap = "quadratic");
     volshow(vol2 .^ 1, Parent = viewerThresholded, ...
             RenderingStyle = "MaximumIntensityProjection", ...
             Colormap = cmap2, ...
-            Alphamap = "linear");
-
+            Alphamap = ramp_alphamap);
+            
+    % Hard coding for now...
+    % viewerThresholded.CameraPosition = [91.4553 -69.5688 -11.5372];
+    % viewerThresholded.CameraTarget = [40.5000 40.5000 31];
+    % viewerThresholded.CameraUpVector = [0.1305 -0.2423 -0.9614];
 end
