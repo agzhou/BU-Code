2353beaef1cce731686a4c20ff3026dd0e39dd58
agzhou <zhoualleng@gmail.com>
2025-10-27 23:29:32 -0400

    added some stuff for trying the atlas registration

--- Allen code/Processing/fUS3D_FC_atlas_registration_test.m
+++ Allen code/Processing/fUS3D_FC_atlas_registration_test.m
@@ -31,4 +31,27 @@ atlas_resized = imresize3(atlas_double, 1/5, 'Method', 'cubic'); % 10 um --> 50 
 % 1. Center the fUS volume (rotationally) and crop to a known size
 % 2. Upsample/resize fUS volume to have isotropic voxel size equal to what
 %    we use for the atlas
-% 3. Manually ___
+% 3. Manually ___ 
+
+%% Temp testing for scatteredInterpolant
+% Might have to do this for every time point???????????????
+tempPDI = PDIallSF(:, :, :, 1);
+tempRigidWarpedPDI = imwarp(tempPDI, fUSmap_50um_rigid_reg.tform);
+
+UP = []; % Ultrasound points
+AP = []; % Atlas points
+for m = 1:size(controlPoints, 1) % Go through each set of control points (rows of the cell array)
+    UP(m, :) = str2num(controlPoints{m, 2});
+    AP(m, :) = str2num(controlPoints{m, 3});
+end
+
+UV = NaN(size(UP, 1), 1); % Ultrasound values (at each control point)
+for m = 1:size(controlPoints, 1) % Go through each set of control points (rows of the cell array)
+    UV(m) = tempPDI(UP(m, 1), UP(m, 2), UP(m, 3));
+end
+
+F = scatteredInterpolant(UP, UV, 'linear');
+
+
+%% random
+test_tform = fitgeotrans(UP, AP, "pwl"); % fitgeotrans(movingPoints, fixedPoints, "pwl")
