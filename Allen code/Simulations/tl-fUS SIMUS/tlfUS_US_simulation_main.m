0ca859786780aa036e5e4d33870c889b0c643fcf
agzhou <zhoualleng@gmail.com>
2025-12-11 02:13:38 -0500

    Update tlfUS_US_simulation_main.m

--- Allen code/Simulations/tl-fUS SIMUS/tlfUS_US_simulation_main.m
+++ Allen code/Simulations/tl-fUS SIMUS/tlfUS_US_simulation_main.m
@@ -4,19 +4,22 @@ addpath([cd, '\MUST'])
 
 %% Set up parameters for the simulated probe
 % param.fc = 13.8889e6; % Center frequency [Hz]
-% param.fc = 3e6; % Center frequency [Hz]
-param.fc = 100; % Center frequency [Hz]
+param.fc = 3e6; % Center frequency [Hz]
+% param.fc = 1e6; % Center frequency [Hz]
 param.bandwidth = 70; % Bandwidth [% of center frequency]
 param.width = 250e-6; % Array element width (x) [m]
 param.height = 250e-6; % Array element height (y) [m]
+param.c = 1540; % Speed of sound [m/s]
 
 % x/y coords of each element
+ne = 32; % # of elements in one dimension, for ne x ne matrix
 pitch = 300e-6; % pitch (in m)
-[xe,ye] = meshgrid(((1:32)-16.5)*pitch);
+% [xe, ye] = meshgrid(((1:32)-16.5)*pitch);
+[xe, ye] = meshgrid(((1:ne) - (ne + 1)/2)*pitch);
 param.elements = [xe(:).'; ye(:).'];
 
 % Transmit apodization
-h = cos(linspace(-pi/4,pi/4,32));
+h = cos(linspace(-pi/4, pi/4, ne));
 h = h'*h;
 param.TXapodization = h(:);
 
@@ -29,9 +32,12 @@ txdel = txdelay3(param, tiltX, tiltY); % in s
 
 % Define volume grid
 n = 32;
-volume_grid.x = linspace(-5e-3, 5e-3, n); % (in m)
-volume_grid.y = linspace(-5e-3, 5e-3, n); % (in m)
-volume_grid.z = linspace(0, 10e-3, 4*n); % (in m)
+volume_grid.x_bounds = [-5e-3, 5e-3];
+volume_grid.y_bounds = [-5e-3, 5e-3];
+volume_grid.z_bounds = [0, 10e-3];
+volume_grid.x = linspace(volume_grid.x_bounds(1), volume_grid.x_bounds(2), n); % (in m)
+volume_grid.y = linspace(volume_grid.y_bounds(1), volume_grid.y_bounds(2), n); % (in m)
+volume_grid.z = linspace(volume_grid.z_bounds(1), volume_grid.z_bounds(2), 4*n); % (in m)
 [x, y, z] = meshgrid(volume_grid.x, volume_grid.y, volume_grid.z);
 
 %%
@@ -54,11 +60,23 @@ plot3(xe*1e2,ye*1e2,xe*0,'b.')
 hold off
 
 %% Simulate backscattered RF signals
+param.fs = 4*param.fc; % Sampling frequency [Hz]
 
 % Define coordinates of scatterers (stored in struct ss)
-ss.x = [0].* 1e3; % x coordinates [m]
-ss.y = [0].* 1e3; % y coordinates [m]
-ss.z = [5].* 1e3; % z coordinates [m]
+ss.x = [0].* 1e-3; % x coordinates [m]
+ss.y = [0].* 1e-3; % y coordinates [m]
+ss.z = [5].* 1e-3; % z coordinates [m]
 ss.Rc = ones(size(ss.x)); % Reflection coefficient = 1
 
 RF = simus3(ss.x, ss.y, ss.z, ss.Rc, [txdel], param);
+
+%% IQ Demodulation and beamforming
+IQ = rf2iq(RF, param);
+
+lambda = param.c/param.fc;
+% beamforming grid
+[bf.x, bf.y, bf.z] = meshgrid(volume_grid.x_bounds(1) : lambda : volume_grid.x_bounds(2), ...
+                              volume_grid.x_bounds(1) : lambda : volume_grid.x_bounds(2), ...
+                              volume_grid.x_bounds(1) : lambda : volume_grid.x_bounds(2));
+
+IQbf = das3(IQ, bf.x, bf.y, bf.z, [txdel], param);
