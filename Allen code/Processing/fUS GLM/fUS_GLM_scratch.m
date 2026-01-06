5186f2299e2087e15ac2a579f5440cdfb88fab1d
agzhou <zhoualleng@gmail.com>
2026-01-05 23:18:47 -0500

    added old files and started my own GLM script

---
+++ Allen code/Processing/fUS GLM/fUS_GLM_scratch.m
@@ -0,0 +1,9 @@
+function [] = fUS_GLM_scratch(data, stim, Aaux, tIncAuto, trange, glmSolveMethod, idxBasis, paramsBasis, driftOrder)
+
+    % I don't want to use the DataClass and snirf stuff
+
+    for 
+
+
+
+end
