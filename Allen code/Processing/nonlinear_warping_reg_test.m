02add66a32e225e2f14199848c15e6d40ef2a999
agzhou <zhoualleng@gmail.com>
2026-02-10 20:03:49 -0500

    Update nonlinear_warping_reg_test.m

--- Allen code/Processing/nonlinear_warping_reg_test.m
+++ Allen code/Processing/nonlinear_warping_reg_test.m
@@ -70,12 +70,12 @@ cpts.F = scatteredInterpolant(cpts.data, cpts.init_disp, 'linear', 'linear');
 %% Apply the scatteredInterpolant to the same grid as the data to get an actual deformation field
 ds = size(AA_template_50um); % Data size
 [y, x, z] = meshgrid(1:ds(1), 1:ds(2), 1:ds(3));
-y = y(:); x = x(:); z = z(:);
+y = y(:); x = x(:); z = z(:); % Make a meshgrid for evaluating the scatteredInterpolant at
 tic
-D = cpts.F(y(:), x(:), z(:));
+D = cpts.F(y(:), x(:), z(:)); % Calculate displacement field
 toc
-testD = reshape(D, ds(1), ds(2), ds(3), 3);
-testD(isnan(testD)) = 0;
+testD = reshape(D, ds(1), ds(2), ds(3), 3); % Reshape the deformation field
+testD(isnan(testD)) = 0; % Remove NaNs
 
 % %% Plot the post-interpolated displacement field
 % figure
@@ -84,11 +84,11 @@ testD(isnan(testD)) = 0;
 % testrange = 1:10000;
 % quiver3(y(testrange), x(testrange), z(testrange), D(testrange, 1), D(testrange, 2), D(testrange, 3))
 
-%%
+%% Warp the ultrasound volume
 testvol = imwarp(PDI_template_rigid_reg, testD, 'cubic');
 volumeViewer(testvol)
 
-%% 
-test2 = PDI_template_rigid_reg(:);
-test3 = reshape(test2, ds(1), ds(2), ds(3));
-volumeViewer(test3)
+% %% 
+% test2 = PDI_template_rigid_reg(:);
+% test3 = reshape(test2, ds(1), ds(2), ds(3));
+% volumeViewer(test3)
