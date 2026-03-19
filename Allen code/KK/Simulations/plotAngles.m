62404259497742f29901f29834c19e8c9f39b563
agzhou <zhoualleng@gmail.com>
2026-03-19 19:01:19 -0400

    Create plotAngles.m

---
+++ Allen code/KK/Simulations/plotAngles.m
@@ -0,0 +1,11 @@
+%% Description: plot direction vector of transmit or receive angles
+function plotAngles(angles)
+    origin = [0, 0, 0];
+    figure; hold on
+    for ai = 1:size(angles, 1)
+        theta = angles(ai, :); % [theta_x, theta_y, theta_z]
+        u = [sin(theta(2)), -sin(theta(1)), cos(theta(1))*cos(theta(2))];
+        plot3([origin(1), origin(1) + u(1)], [origin(2), origin(2) + u(2)], [origin(3), origin(3) + u(3)])
+    end
+
+end
