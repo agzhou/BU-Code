47517cfc2d2251d8d5ae27759c1b17bd5fbd91ca
agzhou <zhoualleng@gmail.com>
2026-07-28 15:57:08 -0400

    Create calcPSFShape2D.m

---
+++ Allen code/Processing/PSF and Phantom stuff/calcPSFShape2D.m
@@ -0,0 +1,65 @@
+%% Description: semi-automatically calculate the ultrasound PSF shape in 3D, taking in simulation data
+
+%% Load simulation data and parameters (assuming Verasonics)
+load('G:\My Drive\Data\PSF Simulations\L22-14v PSF sim - 17 angles from -10 to 10 deg\params.mat')
+load('G:\My Drive\Data\PSF Simulations\L22-14v PSF sim - 17 angles from -10 to 10 deg\PData.mat')
+load('G:\My Drive\Data\PSF Simulations\L22-14v PSF sim - 17 angles from -10 to 10 deg\PSF.mat')
+
+%% Plot the data
+figure; imagesc(abs(IQ)); title('xz plane'); colormap gray
+
+%% Get the position of the point scatterer - assuming there is only one scatterer
+pos_wl = P.Media.MP(1, 1:3); % Position in wavelengths
+pos_m = pos_wl .* P.wl; % [x, y, z] position in meters
+
+%% Get the position of the point scatterer in terms of the reconstructed data coordinates
+
+% This abs(PData.Origin) line is kind of a hack.. fix later if needed
+pos_rc = round((pos_wl + abs(PData.Origin)) ./ PData.PDelta + 1); % Position in recon coords (add 1 because of Matlab indexing)
+
+%% Define some parameters for the Gaussian PSF fit
+gfit_pixel_spacing = 0.01;
+
+
+%% Get x PSF and plot
+xPSF = squeeze(abs(IQ(pos_rc(3), :))); % 1D x PSF
+xPSF_pixel_inds = 1:length(xPSF);
+[xPSF_GF, xPSF_GF_values, xPSF_pixel_inds_GF] = PSFGaussianFit(xPSF_pixel_inds, xPSF, gfit_pixel_spacing); % Fit a Gaussian to the x PSF
+
+% Plot
+figure; plot(xPSF_pixel_inds_GF, xPSF_GF_values, 'LineWidth', 2)
+hold on
+plot(1:length(xPSF), xPSF, ':', 'LineWidth', 2)
+hold off
+title('x PSF and Gaussian fit')
+legend('Gaussian fit', 'Simulated')
+
+%% Get z PSF and plot
+zPSF = squeeze(abs(IQ(:, pos_rc(1)))); % 1D z PSF
+zPSF_pixel_inds = 1:length(zPSF);
+[zPSF_GF, zPSF_GF_values, zPSF_pixel_inds_GF] = PSFGaussianFit(zPSF_pixel_inds, zPSF, gfit_pixel_spacing); % Fit a Gaussian to the x PSF
+
+% Plot
+figure; plot(zPSF_pixel_inds_GF, zPSF_GF_values, 'LineWidth', 2)
+hold on
+plot(1:length(zPSF), zPSF, ':', 'LineWidth', 2)
+hold off
+title('z PSF and Gaussian fit')
+legend('Gaussian fit', 'Simulated')
+
+%% Get 1/e and FWHM values for x, y, z PSFs by using the Gaussian fits
+% FWHM values
+[FWHM_GF_units(1)] = fwhm(xPSF_pixel_inds_GF, xPSF_GF_values);
+[FWHM_GF_units(2)] = NaN;
+[FWHM_GF_units(3)] = fwhm(zPSF_pixel_inds_GF, zPSF_GF_values);
+
+FWHM_wl = FWHM_GF_units .* PData.PDelta ./ gfit_pixel_spacing;
+FWHM_um = FWHM_wl .* P.wl ./ 1e6;
+
+% 1/e values
+[OOE_GF_units(1)] = fw_anymax(xPSF_pixel_inds_GF, xPSF_GF_values, 1/exp(1));
+[OOE_GF_units(2)] = NaN;
+[OOE_GF_units(3)] = fw_anymax(zPSF_pixel_inds_GF, zPSF_GF_values, 1/exp(1));
+
+OOE_wl = OOE_GF_units .* PData.PDelta ./ gfit_pixel_spacing;
+OOE_um = OOE_wl .* P.wl ./ 1e6;
