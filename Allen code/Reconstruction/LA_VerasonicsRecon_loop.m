46ac7a4cee26e454e35005ebde7da91c1219b8fe
agzhou <44214559+agzhou@users.noreply.github.com>
2026-09-04 15:59:35 -0400

    Update LA_VerasonicsRecon_loop.m

--- Allen code/Reconstruction/LA_VerasonicsRecon_loop.m
+++ Allen code/Reconstruction/LA_VerasonicsRecon_loop.m
@@ -259,9 +259,9 @@ end
 
 save([Mcr_savepath, 'PData'], 'PData') % Save the PData structure
 
-% for Mcr_filenum = Mcr_startFile:Mcr_endFile
-% for Mcr_filenum = 142:Mcr_endFile
-for Mcr_filenum = [1]
+for Mcr_filenum = Mcr_startFile:Mcr_endFile
+% for Mcr_filenum = 2:Mcr_endFile
+% for Mcr_filenum = [1]
     tic
 
     load([Mcr_datapath, Mcr_filenameStructure, num2str(Mcr_filenum)], 'RcvData');
