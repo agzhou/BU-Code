ebe1dd0ff757ce32e7e6e29b338c14c45b4cf938
agzhou <zhoualleng@gmail.com>
2026-09-22 13:48:03 -0400

    Updated my 2D fitting function and test script for the general form of my new g1 model

--- Allen code/Processing/vUS/vUS_2D_num_wrapper.m
+++ Allen code/Processing/vUS/vUS_2D_num_wrapper.m
@@ -1,7 +1,20 @@
-function [g1] = vUS_2D_num_wrapper(x, tau, k0, sigma)
+% splitOrNot: boolean to split (or not) the g1 into separate real and
+% imaginary components
+function [g1] = vUS_2D_num_wrapper(x, tau, k0, sigma, splitOrNot)
     % Parse the input
     v_xgp = x(1); v_zgp = x(2); F = x(3); DC = x(4); k = x(5); a = x(6);
+    if nargin < 5
+        splitOrNot = false;
+    end
+    if size(tau, 2) > 1
+        error('tau must be a column vector')
+    end
     
     [fmin, fmax] = calc_f_integration_limits(k, a);
     g1 = DC + F .* integral(vUS_2D_num_vec(x, tau, k0, sigma), fmin, fmax, 'ArrayValued', true);
+
+    if splitOrNot
+        g1 = g1(:);
+        g1 = [real(g1), imag(g1)];
+    end
 end
