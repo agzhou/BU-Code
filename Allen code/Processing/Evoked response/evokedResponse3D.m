38513869b3ae2269e3d73e12e984540719f14b14
agzhou <zhoualleng@gmail.com>
2026-09-23 12:13:24 -0400

    Create evokedResponse3D.m

---
+++ Allen code/Processing/Evoked response/evokedResponse3D.m
@@ -0,0 +1,14 @@
+
+
+%% Choose processed data directory, params, and RF timetags data file
+
+%% Adjust RF timetags
+% Create RF timetag-per-superframe  vector and subtract so it starts at 0
+
+% Crop the stimulus (air puff) signal so it's aligned with the start of data
+% acquisition
+
+% Get corresponding times of "stim on"
+
+% Determine which superframes are present while the stimulus is on
+
