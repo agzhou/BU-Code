178fd18709d81dbcbdb4378413a9a28f4207a1b4
agzhou <zhoualleng@gmail.com>
2026-09-24 18:06:48 -0400

    Update vUS_3D_newmodel.m

--- Allen code/Processing/vUS/vUS_3D_newmodel.m
+++ Allen code/Processing/vUS/vUS_3D_newmodel.m
@@ -531,8 +531,8 @@ v = sqrt(v_tgp.^2 + v_zgp.^2);
 figure; imagesc(squeeze(max(v, [], 1))); clim([0, min(prctile(v(v>0), 99, 'all'), 40e-3)]); colormap turbo; axis equal; axis tight; colorbar
 % figure; imagesc(unstackData(sqrt(Vx0.^2 + Vz0.^2), PP)); clim([0, 0.04]); colormap turbo; axis equal; colorbar
 
-%% Visualize fitted v_zgp
-figure; imagesc(squeeze(max(v_zgp, [], 1))'); colormap(VzCmap); axis equal; axis tight; colorbar; clim([-.030, 0.030]); title("v_{zgp}")
+%% Visualize fitted |v_zgp|
+figure; imagesc(squeeze(max(abs(v_zgp), [], 1))'); colormap(VzCmap); axis equal; axis tight; colorbar; clim([-.030, 0.030]); title("v_{zgp}")
 % figure; imagesc(abs(v_zgp)); colormap(VzCmapDn); axis equal; colorbar
 
 %% Visualize fitted v_tgp
@@ -560,7 +560,7 @@ voxelTimeseriesGUI({g1{3}, g1_model}, v, 'DataNames', {'Data', 'Fit'}, 'ComplexM
 figure; histogram(a(a>0)); title('a')
 figure; histogram(k(k>0)); title('k')
 figure; histogram(v_tgp(v_tgp > 0)); title("v_{tgp}")
-figure; histogram(v_zgp(v_zgp > 0)); title("v_{zgp}")
+figure; histogram(v_zgp(v_zgp ~= 0)); title("v_{zgp}")
 
 %% Testing: visualize vUS results
 vUS_speed = cell(size(vUS));
